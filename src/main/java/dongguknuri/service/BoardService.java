fe2ba8a78ef7a2517f077f286c4661fcfdbb507e
Changsu Park <gd12091@gmail.com>
2024-11-14 20:38:15 +0900

    feat: Board and Comment Service

---
+++ src/main/java/dongguknuri/service/BoardService.java
@@ -0,0 +1,38 @@
+package dongguknuri.service;
+
+import dongguknuri.domain.board.Board;
+import dongguknuri.dto.request.board.CreateBoardDto;
+import dongguknuri.dto.response.board.BoardResponseDto;
+import dongguknuri.repository.board.BoardRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional(readOnly = true)
+    public List<BoardResponseDto> getAllBoards() {
+        return boardRepository.findAll().stream()
+                .map(board -> BoardResponseDto.of(
+                        board.getBoardId(),
+                        board.getName(),
+                        board.getDescription()
+                )).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public boolean createBoard(CreateBoardDto createBoardDto){
+        boardRepository.save(Board.builder()
+                .name(createBoardDto.name())
+                .description(createBoardDto.description())
+                .build());
+
+        return Boolean.TRUE;
+    }
+}
