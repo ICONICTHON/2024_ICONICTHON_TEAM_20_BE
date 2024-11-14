d079cf3ca1ce7a1a9f5e96aec685c275eea2152f
Changsu Park <gd12091@gmail.com>
2024-11-14 15:14:06 +0900

    feat: Post Entity

---
+++ src/main/java/dongguknuri/domain/board/Post.java
@@ -0,0 +1,58 @@
+package dongguknuri.domain.board;
+
+import dongguknuri.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @Column(name = "views")
+    private int views;
+
+    @Column(name = "likes")
+    private int likes;
+
+    @Builder
+    public Post(String title, String content, User user, Board board, int views, int likes) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.board = board;
+        this.views = views;
+        this.likes = likes;
+    }
+}
