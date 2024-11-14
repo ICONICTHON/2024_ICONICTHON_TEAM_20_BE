9a9e93370aac73a49a4d2387fca694ea9fdacf79
Changsu Park <gd12091@gmail.com>
2024-11-14 15:14:21 +0900

    feat: PointHistory Entity

---
+++ src/main/java/dongguknuri/domain/board/PointHistory.java
@@ -0,0 +1,45 @@
+package dongguknuri.domain.board;
+
+import dongguknuri.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "point_history")
+public class PointHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "history_id")
+    private Long historyId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "points", nullable = false)
+    private int points;
+
+    @Column(name = "reason")
+    private String reason;
+
+    @Builder
+    public PointHistory(User user, int points, String reason) {
+        this.user = user;
+        this.points = points;
+        this.reason = reason;
+    }
+}
