2fc83dc4c35639f4a8b13571667524c1885e3c70
Changsu Park <gd12091@gmail.com>
2024-11-14 20:38:04 +0900

    feat: Dto for Board and Comment Service

---
+++ src/main/java/dongguknuri/dto/request/board/CreateBoardDto.java
@@ -0,0 +1,21 @@
+package dongguknuri.dto.request.board;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import lombok.Builder;
+
+@Builder
+public record CreateBoardDto(
+        @JsonProperty("name") String name,
+        @JsonProperty("description") String description
+) implements Serializable {
+    public static CreateBoardDto of(
+            final String name,
+            final String description
+    ) {
+        return CreateBoardDto.builder()
+                .name(name)
+                .description(description)
+                .build();
+    }
+}
---
+++ src/main/java/dongguknuri/dto/response/board/BoardResponseDto.java
@@ -0,0 +1,24 @@
+package dongguknuri.dto.response.board;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import lombok.Builder;
+
+@Builder
+public record BoardResponseDto(
+        @JsonProperty("board_id") Long id,
+        @JsonProperty("name") String name,
+        @JsonProperty("description") String description
+) implements Serializable {
+    public static BoardResponseDto of(
+            final Long id,
+            final String name,
+            final String description
+    ) {
+        return BoardResponseDto.builder()
+                .id(id)
+                .name(name)
+                .description(description)
+                .build();
+    }
+}
